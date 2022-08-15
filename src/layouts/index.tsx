a2a45fed84d0283e6c22e528e41cec2d20ef8f7f
minsgy <alstjrchl97@likelion.org>
2022-08-16 00:24:32 +0900

    ✨ feat: layout component 적용

---
+++ src/layouts/index.tsx
@@ -0,0 +1,19 @@
+import { LAYOUT_MAX_WIDTH } from '@/constants/size'
+import React from 'react'
+import styled from 'styled-components'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export const Layout = ({ children }: LayoutProps) => (
+  <LayoutBase>{children}</LayoutBase>
+)
+
+const LayoutBase = styled('div')`
+  max-width: ${LAYOUT_MAX_WIDTH}px;
+  min-height: 100%;
+  margin: 0 auto;
+  padding: 1em;
+  border-radius: 0.5em;
+`
