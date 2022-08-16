41642e51c8e32347a6a8542719286bf47c5afb60
minsgy <alstjrchl97@likelion.org>
2022-08-17 00:51:46 +0900

    🎨 format: entry point 모음

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export * from './LoginPage'
+export * from './MyProfilePage'
+
+export * from './AuthCodeRequestPage'
+export * from './AuthCodeConfirmPage'
+export * from './ResetPasswordPage'
+
+export * from './UnauthorizedPage'
