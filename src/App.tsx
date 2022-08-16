0e29edaf4e80a5d5c38cab15c02c1f879197f79a
minsgy <alstjrchl97@likelion.org>
2022-08-16 23:06:57 +0900

    ✨ feat: ToastContainer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 import { Routes } from '@/router/Routes'
 import GlobalStyle from '@/styles/global'
+
 const App = () => {
   return (
     <>
+      <ToastContainer />
       <Routes />
       <GlobalStyle />
     </>
