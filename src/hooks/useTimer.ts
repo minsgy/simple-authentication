8f5501dee3e640d775a8ff21417f607f0b900f3e
minsgy <alstjrchl97@likelion.org>
2022-08-17 18:07:33 +0900

    🐛 fix: timer 소수로 변하는 에러 수정

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -3,9 +3,10 @@ import { useState } from 'react'
 import { useInterval } from './useInterval'
 
 export const useTimer = (time: number, onFinish?: () => void) => {
-  const [minute, setMinute] = useState(time / 60 / 1000)
-  const [second, setSecond] = useState(time / 1000 - minute * 60)
-
+  // 180000 = 3 minutes
+  // 60000 = 1 minute
+  const [minute, setMinute] = useState(Math.floor(time / 60000))
+  const [second, setSecond] = useState(Math.floor(time / 1000 - minute * 60))
   const [finished, setFinished] = useState(true)
 
   useInterval(
